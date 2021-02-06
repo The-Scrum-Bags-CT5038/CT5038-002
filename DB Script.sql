5fd52041404607614254cdad890df713fe72b9a8
The-Scrum-Bags-CT5038 <78542876+The-Scrum-Bags-CT5038@users.noreply.github.com>
2021-02-06 10:26:21 +0000

    Update DB Script.sql

--- DB Script.sql
+++ DB Script.sql
@@ -2,32 +2,44 @@
 -- Build user tables
 --
 
+--
+-- NOTES
+-- WASNT SURE WHETHER TO ADD AUTO INCREMENT TO PublicID/MemberID
+-- BUT LOOKED AT CT4009 OO2 WORK AND THAT WASNT IMPLEMENTED
+-- IT WAS DONE IN THE PHP FILE. IF THAT MAKES SENSE
+--
+-- DB FOR ROLES NEEDS SOME WORK ON. IT IS LINKED TO THE REPORT DB IN ERD
+-- SHOULD THIS BE CONNECTED TO COUNCIL MEMBERS INSTEAD?
+--
+
 CREATE TABLE `tbl_public` (
   `PublicID` int NOT NULL PRIMARY KEY,
-	`Email` varchar(20) NOT NULL,
-	`Username` varchar(20) NOT NULL,
-	`Password` varchar(50) NOT NULL,
+  `Email` varchar(20) NOT NULL,
+  `Username` varchar(20) NOT NULL,
+  `Password` varchar(50) NOT NULL,
   'FirstName' varchar(20) NOT NULL,
   'LastName' varchar(20) NOT NULL,
   `DOB` DATE NOT NULL,
   `ReportCount` int,
   `Authenticated` tinyint(1),
-	`RegistrationDate` DATE NOT NULL,
-	`VerificationCode` varchar(16),
+  `RegistrationDate` DATE NOT NULL,
+  `VerificationCode` varchar(16),
 ) ENGINE=InnoDB;
 
 
 
 CREATE TABLE `tbl_councilMember` (
   `MemberID` int NOT NULL PRIMARY KEY,
-	`Email` varchar(20) NOT NULL,
-	`Username` varchar(20) NOT NULL,
-	`Password` varchar(50) NOT NULL,
+  `Email` varchar(20) NOT NULL,
+  `Username` varchar(20) NOT NULL,
+  `Password` varchar(50) NOT NULL,
   'FirstName' varchar(20) NOT NULL,
   'LastName' varchar(20) NOT NULL,
   `DOB` DATE NOT NULL,
   `RoleID` int NOT NULL,
   `Authenticated` tinyint(1),
-	`RegistrationDate` DATE NOT NULL,
-	`VerificationCode` varchar(16),
+  `RegistrationDate` DATE NOT NULL,
+  `VerificationCode` varchar(16),
 ) ENGINE=InnoDB;
+
+
