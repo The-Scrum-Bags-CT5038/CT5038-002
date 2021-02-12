7b8becc3733ccfefdd65787cb97cbb9b181d2bf2
The-Scrum-Bags-CT5038 <78542876+The-Scrum-Bags-CT5038@users.noreply.github.com>
2021-02-12 13:00:50 +0000

    Update DB Script.sql

--- DB Script.sql
+++ DB Script.sql
@@ -19,43 +19,43 @@ DROP  TABLE IF EXISTS `tbl_councilMember`;
 
 CREATE TABLE `tbl_public` (
   `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `Email` varchar(254) NOT NULL,
-  `Username` varchar(20) NOT NULL,
-  `Password` varchar(50) NOT NULL,
-  `FirstName` varchar(20) NOT NULL,
-  `LastName` varchar(20) NOT NULL,
-  `DOB` DATE NOT NULL,
-  `ReportCount` int,
-  `Authenticated` tinyint(1),
-  `CreatedAT` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `UpdateAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `VerificationCode` varchar(16)
+  `email` varchar(254) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
+  `dob` DATE NOT NULL,
+  `reportCount` int(11),
+  `authenticated` tinyint(1),
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `verificationCode` varchar(16)
 ) ENGINE=InnoDB;
 
 
 
 CREATE TABLE `tbl_councilMember` (
   `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `Email` varchar(254) NOT NULL,
-  `Username` varchar(20) NOT NULL,
-  `Password` varchar(50) NOT NULL,
-  `FirstName` varchar(20) NOT NULL,
-  `LastName` varchar(20) NOT NULL,
-  `DOB` DATE NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
+  `dob` DATE NOT NULL,
   `RoleID` int NOT NULL,
-  `Authenticated` tinyint(1),
-  `CreatedAT` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `UpdateAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `VerificationCode` varchar(16)
+  `authenticated` tinyint(1),
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `verificationCode` varchar(16)
 ) ENGINE=InnoDB;
 
 
 CREATE TABLE `tbl_Roles` (
   `id` int NOT NULL PRIMARY KEY,
-  `Title` varchar(20) NOT NULL,
-  `Description` varchar(200) NOT NULL,
-  `CreatedAT` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `UpdateAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `title` varchar(20) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )ENGINE=InnoDB;
 
 ALTER TABLE `tbl_councilMember`
